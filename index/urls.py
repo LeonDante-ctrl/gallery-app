13357de4e1df386b8dfc1d7d7e21fc3a664f5df0
LeonDante-ctrl <maxgamerdu@gmail.com>
2021-03-22 09:18:24 +0300

    set up a system to filter pictures by where it is as well as by category

--- index/urls.py
+++ index/urls.py
@@ -13,4 +13,9 @@ Configure our app url to register the MEDIA_ROOT route.
 urlpatterns = [
     path('', views.index, name='welcome'),
     path('search', views.search_results, name='search_results'),
-   ]
+    path('location/<loc>', views.get_image_by_location, name='location'),
+    path('categories/<cat>', views.get_image_by_category, name='categories'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
