5baf1241a79515f390eb09584170d8848e8b1bc8
LeonDante-ctrl <maxgamerdu@gmail.com>
2021-03-22 12:03:49 +0300

    input a test for my search cases

--- index/tests/test_views.py
+++ index/tests/test_views.py
@@ -23,3 +23,28 @@ class TestIndexView(TestCase):
         self.assertEqual(resp.status_code, 200)
 
         self.assertTemplateUsed(resp, self.template_name)
+
+
+class TestSearchView(TestCase):
+    """
+    test if view url exists at desired location
+    """
+
+    def setUp(self):
+        self.view_name = "search_results"
+        self.route = "/search?image=iron"
+        self.template_name = "results.html"
+
+    def test_view_url_exists_at_desired_location(self):
+        resp = self.client.get(self.route)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        resp = self.client.get(reverse(self.view_name))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse(self.view_name))
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertTemplateUsed(resp, self.template_name)
