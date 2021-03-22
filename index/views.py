af4f4f22227a4ac414161569aaf7ce3013d7d149
LeonDante-ctrl <maxgamerdu@gmail.com>
2021-03-22 11:38:50 +0300

    set parameters for the views based on select categories and locations to apply on the search request added

--- index/views.py
+++ index/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Image
 
 global locations, categories
-locations = ["Metropolis", "Gotham", "Central City", "Avengers mansion", "Bifrost", "Wakanda"]
+locations = ["Mali", "ichiraku", "leaf village", "soya park", "libashire", "Parleys"]
 categories = ["naruto", "dmc"]
 
 
@@ -18,3 +18,20 @@ def index(request):
                    "photos": photos,
                    "locations": locations,
                    "categories": categories})
+
+
+def search_results(request):
+    """
+    Handles search requests
+    """
+    if 'image' in request.GET and request.GET["image"]:
+        query = request.GET.get("image")
+        photos = Image.searched(query)
+        message = f"{query}"
+
+        return render(request, 'results.html',
+                      {"message": message, "photos": photos, "locations": locations, "categories": categories})
+    else:
+        message = "What images are you looking for?"
+        return render(request, 'results.html',
+                      {"message": message, "locations": locations, "categories": categories})
